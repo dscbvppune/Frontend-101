c7bdcc910491016503c7d68bec62c216c5fb2757
Dewansh Rawat <dewanshrawat15@gmail.com>
2020-02-16 10:34:54 +0530

    Create basic layout.dart

---
+++ Flutter/basic layout.dart
@@ -0,0 +1,46 @@
+import "package:flutter/material.dart";
+
+void main(){
+  runApp(
+    Home()
+  );
+}
+
+class Home extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: App(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.orange,
+        accentColor: Colors.orange,
+        highlightColor: Colors.orange,
+        focusColor: Colors.orange
+      )
+    );
+  }
+}
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "DSC BVP Pune",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          )
+        ),
+        centerTitle: true
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.favorite)
+      )
+    );
+  }
+}
